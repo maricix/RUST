74a344acb86e20e93e4b7d83cba17a55138c7680
maricix <75748590+maricix@users.noreply.github.com>
2022-11-04 11:30:27 +0200

    Delete fibonacci/target directory

--- fibonacci/target/debug/deps/fibonacci.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mcaraion.PENTALOG\workspace\rust\fibonacci\target\debug\deps\fibonacci.exe: src\main.rs
-
-C:\Users\mcaraion.PENTALOG\workspace\rust\fibonacci\target\debug\deps\fibonacci.d: src\main.rs
-
-src\main.rs:
--- fibonacci/target/debug/fibonacci.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mcaraion.PENTALOG\workspace\rust\fibonacci\target\debug\fibonacci.exe: C:\Users\mcaraion.PENTALOG\workspace\rust\fibonacci\src\main.rs
